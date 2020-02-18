23bfb2630dfd2e5981e17c42298319b6d60913c5
leo2c <z429882801@qq.com>
2020-02-18 16:36:32 +0800

    修改CI配置 - 上传了CI的配置文件 - 添加SmokeTest测试类，一个冒烟测试 - 添加Main类，其中包含了使用Apche HttpClient 进行网页链接操作 - 修改了README.md项目介绍

---
+++ src/main/java/com/github/hcsp/Main.java
@@ -0,0 +1,29 @@
+package com.github.hcsp;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
+
+import java.io.IOException;
+
+public class Main {
+    public static final String html = "";
+    public static void main(String[] args) throws IOException {
+        CloseableHttpClient httpclient = HttpClients.createDefault();
+        HttpGet httpGet = new HttpGet("http://targethost/homepage");
+        CloseableHttpResponse response1 = httpclient.execute(httpGet);
+        try {
+            System.out.println(response1.getStatusLine());
+            HttpEntity entity1 = response1.getEntity();
+            // do something useful with the response body
+            // and ensure it is fully consumed
+            EntityUtils.consume(entity1);
+        } finally {
+            response1.close();
+        }
+
+    }
+}
