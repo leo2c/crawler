7ed8f53066e1ab255f930fd9ac984e60ed91da90
leo2c <z429882801@qq.com>
2020-02-22 19:23:13 +0800

    优化了代码，添加了flayway插件 小步提交

---
+++ src/main/resources/db/migration/V1__Create_tables.sql
@@ -0,0 +1,21 @@
+create table LINKS_TO_BE_PROCESSED
+(
+	link varchar(2000)
+);
+create table LINKS_ALREADY_PROCESSED
+(
+	link varchar(2000)
+);
+create table LINKS_FAILED_PROCESSED
+(
+	link varchar(2000)
+);
+create table NEWS
+(
+	id bigint primary key auto_increment,
+	title text,
+	content text,
+	url varchar(1000),
+	created_at timestamp,
+	modified_at timestamp,
+);
